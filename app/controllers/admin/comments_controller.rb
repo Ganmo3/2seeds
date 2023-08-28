2aaad843f4e3599fbdc899b72f062b41f57aa726
Ganmo <m.kato1201@gmail.com>
2023-08-28 17:39:40 +0000

    fix deploy setting

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,9 +1,9 @@
 class Admin::CommentsController < ApplicationController
   def destroy
-  @post = Post.find(params[:post_id])
-  @comment = Comment.find(params[:id])
-  @comment.destroy
-  @comments = Comment.where(post_id: params[:post_id]).order(created_at: :desc)
-  redirect_to request.referer, notice: "コメントを削除しました。"
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    @comments = Comment.where(post_id: params[:post_id]).order(created_at: :desc)
+    redirect_to request.referer, notice: "コメントを削除しました。"
   end
 end
