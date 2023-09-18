e2ba5d3b4a1963e0c3bb0a1e2863737996210b15
Ganmo <m.kato1201@gmail.com>
2023-09-18 20:15:41 +0000

    [fix] layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,21 +53,19 @@ end
 
 # ユーザー作成
 nicknames.each_with_index do |nickname, index|
-  account = "oden#{index + 1}"
+  account = "2seeds#{index + 1}"
   find_or_create_user(account, nickname, tags)
 end
 
 # 投稿の作成メソッド
 def create_posts_for_user_with_ordered_dates(user, count, status_options, tags)
-  # ユーザーの最初の投稿日を設定
-  initial_date = Time.now - (count - 1).days  # 最新の投稿が今日となるように設定
+  initial_date = Time.now - (count - 1).days  
   
   count.times do |i|
     title = "投稿#{i + 1}"
     body = "サンプルの投稿#{i + 1}です。\n新鮮な野菜、チーズ、ハムなどお好みの具材を挟んだホットサンドは、軽食からランチまで幅広く楽しめる美味しい料理です。\nシンプルな食材で手軽に作れ、サクサクの食感ととろけるチーズの組み合わせが絶妙です。\nぜひ自宅で作ってみて、美味しいひとときを楽しんでみてください。"
     link = "https://www.youtube.com/watch?v=eYreBz3S7f8"
     status = status_options.sample
-    impressions_count = rand(100..1000)
     tag_list = tags.sample(2)
 
     # 日付を順番に遅らせる
@@ -83,16 +81,14 @@ def create_posts_for_user_with_ordered_dates(user, count, status_options, tags)
       p.body = body
       p.link = link
       p.status = status
-      p.impressions_count = impressions_count
       p.created_at = post_date
       p.updated_at = post_date
-      p.tag_list.add(tag_list)  # タグを追加する
+      p.tag_list.add(tag_list)  
     end
     
     puts "Creating post with title: #{title}, user_account: #{user.account}, tag_list: #{tag_list}"
   end
 end
-
 
 # ユーザーごとに異なるステータス割り当て
 User.where.not(account: 'guest').each do |user|
@@ -124,9 +120,8 @@ user_comments = [
 
 users.each do |user|
   posts.each do |post|
-    # 各ユーザーが1つの投稿に対して最大1つのコメントを生成
     num_comments = rand(0..1)
-    next if num_comments.zero?  # コメントを生成しない場合はスキップ
+    next if num_comments.zero?  
 
     comment_text = user_comments.sample
     comment = Comment.find_or_create_by!(
@@ -148,7 +143,7 @@ end
 # リレーションシップの作成
 Relationship.delete_all
 users.each do |user|
-  following_users = users - [user]  # 自分自身以外のユーザーから選ぶ
+  following_users = users - [user]  
   following_users.shuffle.take(rand(1..4)).each do |following_user|
     Relationship.find_or_create_by!(
       follower_id: user.id,
